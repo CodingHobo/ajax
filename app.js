46e31b526258f03b15296d2f5d5c5bf4e9e5dd8f
Tanya Shylock <tourtanya@gmail.com>
2023-04-11 17:19:03 -0600

    edit index, app

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Let's get this party started!");
+"use strict";
+
+let searchTerm = $("#search");
+
+async function getTerm() {
+  let response = await axios.get("api.giphy.com/v1/gifs", { q: searchTerm });
+  console.log("get result=", response.data);
+}
+$("#submit").on("click", getTerm());
+//console.log("Let's get this party started!");
